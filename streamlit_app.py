f819d587e544e62ad70b7a8271a4fe8372c75dfb
teddyjack5 <teddyjack5@gmail.com>
2026-04-28 15:18:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -251,7 +251,7 @@ def get_recall_question(user_id):
 # ==============================================================================
 st.sidebar.title("設定")
 user_id = st.sidebar.text_input("User ID")
-mode = st.sidebar.radio("模式", ["測驗", "學習進度分析", "新增單字庫"]) # 新增分析選項
+mode = st.sidebar.radio("模式", ["測驗", "學習進度分析", "新增單字庫","🧠 記憶強化","🔁 今日複習"]) # 新增分析選項
 old_practice_mode = st.session_state.get("last_practice_mode")
 practice_mode = st.sidebar.selectbox("練習模式", ["單字", "填空", "錯題"])
 if old_practice_mode != practice_mode:
